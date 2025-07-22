b51aebb0df31a6a3d2aa262915d3155d2a257aeb
syed-gufran <tazeema07@gmail.com>
2025-07-22 19:06:35 +0530

    added setup and proctfile to deploy on heroku

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo"\
+[general]\n\
+email = \"your_email@example.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
